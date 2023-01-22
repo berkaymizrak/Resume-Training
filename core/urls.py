6d2d4bdd0ecfaacffd2b962b7c370c715061385b
Berkay MIZRAK <mizrakby@gmail.com>
2023-01-22 21:37:19 +0300

    feat: create contact page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact', contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
