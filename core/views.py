1ba38c4a0cd7877366acd6c9d1391d326461769a
Berkay MIZRAK <mizrakby@gmail.com>
2023-02-23 22:48:06 +0300

    refactor: apply image setting

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import GeneralSetting
+from core.models import GeneralSetting, ImageSetting
 
 # Create your views here.
 
@@ -13,6 +13,11 @@ def index(request):
     about_myself_welcome = GeneralSetting.objects.get(name='about_myself_welcome').parameter
     about_myself_footer = GeneralSetting.objects.get(name='about_myself_footer').parameter
 
+    # Images
+    header_logo = ImageSetting.objects.get(name='header_logo').file
+    home_banner_image = ImageSetting.objects.get(name='home_banner_image').file
+    site_favicon = ImageSetting.objects.get(name='site_favicon').file
+
     context = {
         'site_title': site_title,
         'site_keywords': site_keywords,
@@ -22,5 +27,8 @@ def index(request):
         'home_banner_description': home_banner_description,
         'about_myself_welcome': about_myself_welcome,
         'about_myself_footer': about_myself_footer,
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon,
     }
     return render(request, 'index.html', context=context)
