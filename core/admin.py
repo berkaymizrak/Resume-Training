3e2c4629f20027d4db6e41145182e7030275456d
Berkay MIZRAK <mizrakby@gmail.com>
2023-02-23 21:40:20 +0300

    feat: add general setting model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import *
 
 # Register your models here.
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'parameter', 'updated_date', 'created_date']
+    search_fields = ['name', 'description', 'parameter']
+    list_editable = ['description', 'parameter']
+
+    class Meta:
+        model = GeneralSetting
--- core/models.py
+++ core/models.py
@@ -2,27 +2,43 @@ from django.db import models
 
 # Create your models here.
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     name = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Name',
+        help_text='This is variable of the setting.',
     )
     description = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Description',
+        help_text='',
     )
     parameter = models.CharField(
         default='',
         max_length=254,
         blank=True,
+        verbose_name='Parameter',
+        help_text='',
     )
     updated_date = models.DateTimeField(
         blank=True,
         auto_now=True,
+        verbose_name='Updated Date'
     )
     created_date = models.DateTimeField(
         blank=True,
         auto_now_add=True,
+        verbose_name='Created Date'
     )
+
+    def __str__(self):
+        return f'General Setting: {self.name}'
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name', )
