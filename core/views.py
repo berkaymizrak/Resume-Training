a37217579f18bc9bfb1e0b58d062e3a550daa070
Berkay MIZRAK <mizrakby@gmail.com>
2023-02-23 23:29:15 +0300

    refactor: apply experiences to frontend

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import GeneralSetting, ImageSetting, Skill
+from core.models import GeneralSetting, ImageSetting, Skill, Experience
 
 # Create your views here.
 
@@ -21,6 +21,8 @@ def index(request):
     # Skills
     skills = Skill.objects.all()
 
+    experiences = Experience.objects.all().order_by('-start_date')
+
     context = {
         'site_title': site_title,
         'site_keywords': site_keywords,
@@ -34,5 +36,6 @@ def index(request):
         'home_banner_image': home_banner_image,
         'site_favicon': site_favicon,
         'skills': skills,
+        'experiences': experiences,
     }
     return render(request, 'index.html', context=context)
